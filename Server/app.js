1fb2fc4478c2b662de8cbd262fb1303a2da3db6a
Yoav Stezki <yoavs@pinkpark.com>
2018-07-26 02:45:29 +0300

    setup web-shopping-store project

---
+++ Server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+//Port Number
+const port = 8080;
+
+const app = express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(express.static(path.join(__dirname, 'Client/src')))
+    .use(bodyParser.json())
+    .listen(port, () => console.log(`Server start on port ${port}`));
+
